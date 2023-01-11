12bf941f5092b8f4ebec5888162c6b641ef47fa0
Alberto Bautista <dachb101@gmail.com>
2023-01-11 16:29:53 -0600

    Update

--- components/common/ImageHeader/ImageHeader.tsx
+++ components/common/ImageHeader/ImageHeader.tsx
@@ -12,7 +12,7 @@ export const ImageHeader = () => {
         backgroundImage: `url(${profilePic.src})`,
       }}
     >
-      <article className={`relative pb-0 pr-0`}>
+      <article className="pb-0 pr-0 ">
         <div className="flex items-center">
           <div className="flex justify-center w-full p-3 bg-cvr-green/75">
             <p className="text-3xl font-semibold tracking-wider text-white uppercase font-title">
@@ -20,14 +20,6 @@ export const ImageHeader = () => {
             </p>
           </div>
         </div>
-        <div
-          className={
-            'z-0 absolute inset-0 opacity-10 bg-center bg-[size:150%] group-hover:bg-[size:200%] transition '
-          }
-          //   style={`background-image:url('${Astro.props.image}')`}
-          //   style={{ backgroundImage: `url('${profilePic}')` }}
-        />
-        {/* <CardBackgroundTeam image={team.imageWhite} /> */}
       </article>
     </section>
   );
